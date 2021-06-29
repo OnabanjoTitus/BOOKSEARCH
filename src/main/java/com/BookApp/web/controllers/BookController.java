5abb97b43cf4108161dfe815a30b55503c547ccc
Onabanjo Titus <titus.onabanjo@arikair.com>
2021-06-29 11:44:59 +0100

    controller updates

--- src/main/java/com/BookApp/web/controllers/BookController.java
+++ src/main/java/com/BookApp/web/controllers/BookController.java
@@ -6,6 +6,7 @@ import com.BookApp.web.exceptions.BookNameCannotBeEmptyException;
 import com.BookApp.web.exceptions.BookNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,9 +14,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import java.util.List;
 import java.util.stream.Collectors;
-
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @Slf4j
 @RestController
 @RequestMapping("/bookSearch")
@@ -25,17 +28,7 @@ public class BookController {
     @GetMapping("")
     private ResponseEntity<?>bookList(){
         List<Book>books=  bookServices.findAllBooks();
-//                .stream()
-//                .map(book -> {
-//                    try {
-//                          return book.add(linkTo(methodOn(BookController.class).findByName(book.getTitle())).withSelfRel(),
-//                                linkTo(methodOn(BookController.class).findAll()).withSelfRel());
-//                    } catch (BookNotFoundException | BookNameCannotBeEmptyException e) {
-//                        e.printStackTrace();
-//                        return null;
-//                    }
-//
-//                }).collect(Collectors.toList());
+
         log.info("The list of books returned are-->{}",books);
         return ResponseEntity.ok(books);
     }
@@ -43,12 +36,10 @@ public class BookController {
     private ResponseEntity<?> findBooksByName(@PathVariable("id") String bookName) throws BookNotFoundException, BookNameCannotBeEmptyException {
         log.info("The searchKeyword is -->{}",bookName);
         List<Book> bookList = bookServices.findByName(bookName);
-//        EntityModel<Book>bookEntityModel=EntityModel.of(
-//                bookList,linkTo(methodOn(BookController.class)
-//                        .findBooksByName(bookName)).
-//                        withSelfRel(),linkTo(methodOn(BookController.class).
-//                        findAll()).withRel("books"));
+        EntityModel<List<Book>> bookEntityModel=EntityModel.of(
+                bookList,linkTo(methodOn(BookController.class).
+                        bookList()).withRel("books"));
         log.info("The list of books or book related to the keyword -->{} are -->{}",bookName,bookList);
-        return new ResponseEntity<>(bookList, HttpStatus.OK);
+         return ResponseEntity.ok(bookList);
     }
 }
